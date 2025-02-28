1b17bb8a471bd9046d85377f5f99934218e97927
Marat Gafiiatullin <marat.webdev@gmail.com>
2025-02-28 08:39:49 +0100

    Add a version flag, update packages

--- go.mod
+++ go.mod
@@ -1,5 +1,7 @@
 module github.com/maratig/trace_analyzer
 
-go 1.22
+go 1.23.0
 
-require golang.org/x/exp v0.0.0-20240904232852-e7e105dedf7e
+toolchain go1.23.6
+
+require golang.org/x/exp v0.0.0-20250218142911-aa4b98e5adaa
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"os"
 	"os/signal"
 
@@ -10,7 +11,19 @@ import (
 	"github.com/maratig/trace_analyzer/internal/server"
 )
 
+var (
+	appName = "trace_analyzer"
+	version = "0.0.1"
+)
+
 func main() {
+	ver := flag.Bool("version", false, "print version and exit")
+	flag.Parse()
+	if ver != nil && *ver {
+		fmt.Println(appName, "version", version)
+		os.Exit(0)
+	}
+
 	ctx, _ := signal.NotifyContext(context.Background(), os.Kill, os.Interrupt)
 	port := flag.Int("port", 0, "port to be used in REST endpoint")
 	endpointConnectionWait := flag.Int(
