b43d3f715bbe69e5d33909e03029acb73967ec13
Marat Gafiiatullin <marat.webdev@gmail.com>
2025-09-28 12:03:49 +0200

    Add an integration test for HeapProcess

--- cmd/external-test-app.go
+++ cmd/external-test-app.go
@@ -16,30 +16,37 @@ const defaultExtTestAppAddr = "127.0.0.1:11000"
 var extTestAppCmd = &cobra.Command{
 	Use:   "ext-test-app",
 	Short: "Run an external application to be used for testing the analyzer",
+	PreRun: func(cmd *cobra.Command, args []string) {
+		ctx, _ := signal.NotifyContext(context.Background(), os.Kill, os.Interrupt)
+		cmd.SetContext(ctx)
+	},
 	Run: func(cmd *cobra.Command, args []string) {
 		addr, err := cmd.Flags().GetString("addr")
 		if err != nil {
 			panic(fmt.Sprintf("failed to parse external test app addr; %v", err))
 		}
 
-		if addr == "" {
-			addr = defaultExtTestAppAddr
+		if err := RunExtTestApp(cmd.Context(), addr); err != nil {
+			panic(fmt.Sprintf("RunExtTestApp command failed; %v", err))
 		}
-
-		runExtTestApp(addr)
 	},
 }
 
 func initExtTestAppCmdFlags() {
-	extTestAppCmd.Flags().StringP("addr", "a", "", "Address to be exposed for clients")
+	extTestAppCmd.Flags().StringP("addr", "a", "", "Address to be exposed to clients")
 }
 
-func runExtTestApp(addr string) {
-	ctx, _ := signal.NotifyContext(context.Background(), os.Kill, os.Interrupt)
-	if err := extApp.RunExternalApp(ctx, addr); err != nil {
-		panic(fmt.Sprintf("failed to run external test app; %v", err))
+// RunExtTestApp is public for running integration tests
+func RunExtTestApp(ctx context.Context, addr string) error {
+	if addr == "" {
+		addr = defaultExtTestAppAddr
 	}
+	if err := extApp.RunExternalApp(ctx, addr); err != nil {
+		return fmt.Errorf("failed to run external app on addr=%s; %w", addr, err)
+	}
+
 	fmt.Printf("External test app is running on %s\n", addr)
 
 	<-ctx.Done()
+	return nil
 }
---
+++ integration_tests/heap-process_test.go
@@ -0,0 +1,33 @@
+package integration_tests
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/maratig/trace_analyzer/cmd"
+	"github.com/maratig/trace_analyzer/internal/service"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestProfiles(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	t.Cleanup(cancel)
+
+	addr := "127.0.0.1:15000"
+	go func() {
+		err := cmd.RunExtTestApp(ctx, addr)
+		require.NoError(t, err)
+	}()
+
+	hp, err := service.NewHeapProcessor(0, "http://"+addr+"/debug/pprof/heap")
+	require.NoError(t, err)
+	err = hp.Run(ctx)
+	require.NoError(t, err)
+
+	time.Sleep(5 * time.Second)
+	profiles := hp.Profiles()
+	assert.NotEmpty(t, profiles)
+}
